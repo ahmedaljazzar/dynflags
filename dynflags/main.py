8c9a4f983f0585362104944d958cbb31c06bf470
Ahmed Jazzar <ajazzar@clearcareonline.com>
2019-06-04 15:47:10 -0700

    Custom config onject

--- dynflags/main.py
+++ dynflags/main.py
@@ -16,69 +16,93 @@ import logging
 from .exceptions import *
 
 DEFAULT_TABLE_SPEC = {"BillingMode": 'PAY_PER_REQUEST'}
-DEFAULT_LIST_KEY = '__default__'
 
 
 def write_only(func):
     def block_read_only(self, *args, **kwargs):
-        if self._read_only:
+        if self._config.read_only:
             raise ReadOnlyException()
         return func(self, *args, **kwargs)
 
     return block_read_only
 
 
+def merge(dict1, dict2):
+    res = {**dict1, **dict2}
+    return res
+
+
+class Config(dict):
+    def __init__(self, table_name, *args, **kwargs):
+        default_conf = {
+            'boto_config': {'region_name': 'us-east-1'},
+            'table_name': table_name,
+            'table_config': DEFAULT_TABLE_SPEC.copy(),
+            'dynamo': None,
+            'cache': None,
+            'cached_table_obj': None,
+            'autocreate_table': False,
+            'read_only': True,
+            'consistent_reads': False,
+            'logger': logging.getLogger(__name__),
+            'robust': False,
+            'default_list_key': '__default__'
+        }
+
+        configurations = merge(default_conf, kwargs)
+        super(Config, self).__init__(*args, **configurations)
+        for k, v in configurations.items():
+            self[k] = v
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(Config, self).__setitem__(key, value)
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(Config, self).__delitem__(key)
+        del self.__dict__[key]
+
+
 class DynFlagManager:
-    def __init__(self,
-                 table_name,
-                 boto_config={'region_name': 'us-east-1'},
-                 table_config=None,
-                 cache=None,
-                 autocreate_table=False,
-                 read_only=True,
-                 consistent_reads=False,
-                 logger=None,
-                 robust=False):
-        self._table_name = table_name
-        self._autocreate_table = autocreate_table
-        self._boto_config = boto_config
-        self._dynamo = None
-        self._cached_table_obj = None
-        self._cache = cache
-        self._table_config = table_config or DEFAULT_TABLE_SPEC.copy()
-        self._read_only = read_only
-        self._consistent_reads = consistent_reads
-        self._logger = logger or logging.getLogger(__name__)
-        self._robust = robust
+    def __init__(self, table_name, **kwargs):
+        self._config = Config(table_name, **kwargs)
 
     @property
     def dynamo(self):
-        if not self._dynamo:
-            self._dynamo = boto3.resource('dynamodb', **self._boto_config)
-        return self._dynamo
+        if not self._config.dynamo:
+            self._config.dynamo = boto3.resource('dynamodb', **self._config.boto_config)
+        return self._config.dynamo
 
     @property
     def table(self):
-        if self._cached_table_obj:
-            self._logger.debug('Returning cached table object')
-            return self._cached_table_obj
-        self._logger.debug('Table object not cached')
-        self._cached_table_obj = self.dynamo.Table(self._table_name)
+        if self._config.cached_table_obj:
+            self._config.logger.debug('Returning cached table object')
+            return self._config.cached_table_obj
+        self._config.logger.debug('Table object not cached')
+        self._config.cached_table_obj = self.dynamo.Table(self._config.table_name)
         try:
-            self._cached_table_obj.creation_date_time
+            self._config.cached_table_obj.creation_date_time
         except self.dynamo.meta.client.exceptions.ResourceNotFoundException as exc:
-            self._logger.debug('Table does not exist')
-            if self._autocreate_table:
+            self._config.logger.debug('Table does not exist')
+            if self._config.autocreate_table:
                 self._initialize_table()
             else:
                 raise exc
-        return self._cached_table_obj
+        return self._config.cached_table_obj
 
     def _initialize_table(self):
         # create the table
-        self._logger.debug('Creating DynamoDB Table: %s, %s' %
-                           (self._table_name, self._table_config))
-        table = self.dynamo.create_table(TableName=self._table_name,
+        self._config.logger.debug('Creating DynamoDB Table: %s, %s' %
+                           (self._config.table_name, self._config.table_config))
+        table = self.dynamo.create_table(TableName=self._config.table_name,
                                          KeySchema=[{
                                              "AttributeName": "arguments",
                                              "KeyType": "HASH"
@@ -89,10 +113,10 @@ class DynFlagManager:
                                              "AttributeType":
                                                  "S"
                                          }],
-                                         **self._table_config)
+                                         **self._config.table_config)
         table.meta.client.get_waiter('table_exists').wait(
-            TableName=self._table_name)
-        self._logger.debug('DynamoDB Table Created')
+            TableName=self._config.table_name)
+        self._config.logger.debug('DynamoDB Table Created')
 
     def _gen_dynamo_key_from_key(self, key):
         return {"arguments": key}
@@ -104,6 +128,11 @@ class DynFlagManager:
             raise InvalidArgumentValueTypeException('Argument values must be strings')
 
     def _validate_flags(self, flag_names):
+        if isinstance(flag_names, list):
+            if not all(isinstance(x, string_types) for x in flag_names):
+                raise InvalidFlagNameTypeException('Flags names must be strings')
+            return
+
         if not all(isinstance(x, string_types) for x in flag_names.keys()):
             raise InvalidFlagNameTypeException('Flags names must be strings')
 
@@ -116,13 +145,13 @@ class DynFlagManager:
 
     def _gen_key_from_args(self, arguments):
         if not arguments:
-            return DEFAULT_LIST_KEY
+            return self._config.default_list_key
         self._validate_arguments(arguments)
         arglist = list(arguments.items())
         arglist.sort(key=lambda x: x[0])
         key_fragments = ("%s=%s" % (x, str(y)) for x, y in arglist)
         key = ';'.join(key_fragments)
-        self._logger.debug('Generated key from args: %s, %s' %
+        self._config.logger.debug('Generated key from args: %s, %s' %
                            (arguments, key))
         return key
 
@@ -134,29 +163,29 @@ class DynFlagManager:
         return args
 
     def _query_dynamodb_for_flags_for_key(self, key):
-        self._logger.debug('Querying DynamoDB for key: %s' %
+        self._config.logger.debug('Querying DynamoDB for key: %s' %
                            self._gen_dynamo_key_from_key(key))
         item = self.table.get_item(Key=self._gen_dynamo_key_from_key(key),
-                                   ConsistentRead=self._consistent_reads)
+                                   ConsistentRead=self._config.consistent_reads)
 
         flags = item.get('Item', {}).get('flags', {})
-        self._logger.debug('Found the following flags for key: %s, %s' %
+        self._config.logger.debug('Found the following flags for key: %s, %s' %
                            (key, flags))
 
         return flags
 
     def _get_flags_for_key(self, key, use_cache=True):
-        if use_cache and self._cache:
-            self._logger.debug('Checking cache for key: %s' % key)
-            cached_flags = self._cache.get(key)
+        if use_cache and self._config.cache:
+            self._config.logger.debug('Checking cache for key: %s' % key)
+            cached_flags = self._config.cache.get(key)
             if cached_flags:
-                self._logger.debug('Returning cached flags for key: %s, %s' %
+                self._config.logger.debug('Returning cached flags for key: %s, %s' %
                                    (key, cached_flags))
                 return cached_flags
         flags = self._query_dynamodb_for_flags_for_key(key)
-        if use_cache and self._cache:
-            self._logger.debug('Setting cache for key: %s, %s' % (key, flags))
-            self._cache.set(key, flags)
+        if use_cache and self._config.cache:
+            self._config.logger.debug('Setting cache for key: %s, %s' % (key, flags))
+            self._config.cache.set(key, flags)
         return flags
 
     def _get_flags_for_args(self, arguments, use_cache=True):
@@ -200,10 +229,6 @@ class DynFlagManager:
                 ExpressionAttributeValues={':flags': flag_names}
             )
 
-    def _merge(self, dict1, dict2):
-        res = {**dict1, **dict2}
-        return res
-
     @write_only
     def add_flag(self, flag_name, enabled, arguments={}):
         flag_names = {flag_name: enabled}
@@ -223,11 +248,11 @@ class DynFlagManager:
         self._manipulate_flags(flag_names, arguments, 'REMOVE')
 
     def is_active(self, *args, **kwargs):
-        if self._robust:
+        if self._config.robust:
             try:
                 return self._is_active(*args, **kwargs)
             except Exception as exc:
-                self._logger.error(str(exc))
+                self._config.logger.error(str(exc))
                 return False
         else:
             return self._is_active(*args, **kwargs)
@@ -235,7 +260,7 @@ class DynFlagManager:
     def get_item_for_key(self, key):
         result = self.table.get_item(
             Key=self._gen_dynamo_key_from_key(key),
-            ConsistentRead=self._consistent_reads
+            ConsistentRead=self._config.consistent_reads
         )
 
         item = result['Item']
@@ -246,13 +271,13 @@ class DynFlagManager:
         return self.get_item_for_key(key)
 
     def get_default_flags(self):
-        return self._get_flags_for_key(DEFAULT_LIST_KEY)
+        return self._get_flags_for_key(self._config.default_list_key)
 
     def get_flags_for_args(self, arguments):
         default_flags = self.get_default_flags()
         item_flags = self._get_flags_for_args(arguments)
 
-        return self._merge(default_flags, item_flags)
+        return merge(default_flags, item_flags)
 
     def get_all_flags(self):
         response = self.table.scan()
